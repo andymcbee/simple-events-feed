b793512a46e30f4ec38c3cffbbf21e40a73d5b1e
Andrew <armcburn@gmail.com>
2022-08-24 12:25:18 -0400

    Prepare for deployment

--- backend/routes/eventsRoute.js
+++ backend/routes/eventsRoute.js
@@ -5,12 +5,13 @@ import {
   deleteEvent,
   getEvents,
 } from "../controllers/event.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/create", createEvent);
-router.post("/update/:eventId", updateEvent);
-router.post("/delete/:eventId", deleteEvent);
+router.post("/create", auth, createEvent);
+router.post("/update/:eventId", auth, updateEvent);
+router.post("/delete/:eventId", auth, deleteEvent);
 router.get("/:organizationId/:period", getEvents); //period can equal: all, past or future
 
 export default router;
